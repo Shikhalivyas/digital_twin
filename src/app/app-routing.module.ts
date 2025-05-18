7fe39fd341008a0f7a668691442c627c2ac97bbd
Shikhalivyas <bhattshikhali@gmail.com>
2025-05-18 11:09:11 +0530

    changes for graph

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { TeammembersComponent } from './teammembers/teammembers.component';
+import { EnergyChartComponent } from './energy-chart-component/energy-chart-component.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path:'team',component:TeammembersComponent}
+  { path:'team',component:TeammembersComponent},
+  {path: 'graph', component:EnergyChartComponent},
 ];
 
 @NgModule({
